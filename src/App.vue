e8a66755dd6f27fffcda115bfe205d7dd98804d3
ajuner <106791576@qq.com>
2021-08-26 00:21:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    return {
+      msg: "hello world3",
+    };
+  },
+});
+</script>
+
+<style>
+</style>
